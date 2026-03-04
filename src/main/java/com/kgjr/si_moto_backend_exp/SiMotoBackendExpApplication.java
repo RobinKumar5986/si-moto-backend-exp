aa6b0f680e5b5a8c118bc8d25cf7e0fcf62bd7c6
Robin Kumar <rkrobin6550@gmail.com>
2026-03-05 00:09:03 +0530

    initial commit

---
+++ src/main/java/com/kgjr/si_moto_backend_exp/SiMotoBackendExpApplication.java
@@ -0,0 +1,13 @@
+package com.kgjr.si_moto_backend_exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiMotoBackendExpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiMotoBackendExpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgjr/si_moto_backend_exp/SiMotoBackendExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgjr.si_moto_backend_exp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiMotoBackendExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
