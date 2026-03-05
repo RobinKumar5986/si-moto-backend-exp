741c4c50b30ddde1667484585307bbdc1384eda5
Robin Kumar <rkrobin6550@gmail.com>
2026-03-06 01:46:19 +0530

    Basic Setup and stuff is done

---
+++ src/main/java/com/kgjr/si_moto_backend_exp/database/Video.java
@@ -0,0 +1,41 @@
+package com.kgjr.si_moto_backend_exp.database;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.locationtech.jts.geom.Point;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "videos")
+@Getter
+@Setter
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Column(name = "company_id", nullable = false)
+    private String companyId;
+
+    @Column(columnDefinition = "geometry(Point, 4326)")
+    private Point location;
+
+    private String country;
+    private String state;
+
+    private String fileExtension;
+
+    @ElementCollection
+    @CollectionTable(name = "video_tags", joinColumns = @JoinColumn(name = "video_id"))
+    @Column(name = "tag")
+    private List<String> tags;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
